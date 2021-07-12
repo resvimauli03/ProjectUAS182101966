d7b7326226afa934d7d2fb9343b3cfc0ecc4d3b5
resvimauli03 <resvimaulida4@gmail.com>
2021-07-12 22:16:48 +0700

    UASmobileLanjutan

---
+++ app/src/main/java/com/resvi/uasresvi/EditActivity.java
@@ -0,0 +1,100 @@
+package com.resvi.uasresvi;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.resvi.uasresvi.Model.PostPutDelUser;
+import com.resvi.uasresvi.Rest.ApiClient;
+import com.resvi.uasresvi.Rest.ApiInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EditActivity extends AppCompatActivity {
+
+    EditText edtId, edtNama1, edtNama2,edtEmail;
+    Button btUpdate, btDelete, btBack;
+    ApiInterface mApiInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        edtId = (EditText) findViewById(R.id.edtId);
+        edtNama1 = (EditText) findViewById(R.id.edtNama1);
+        edtNama2 = (EditText) findViewById(R.id.edtNama2);
+        edtNama2 = (EditText) findViewById(R.id.edtNama2);
+        Intent mIntent = getIntent();
+        edtId.setText(mIntent.getStringExtra("Id"));
+        edtId.setTag(edtId.getKeyListener());
+        edtId.setKeyListener(null);
+        edtNama1.setText(mIntent.getStringExtra("First Name"));
+        edtNama2.setText(mIntent.getStringExtra("Last Name"));
+        edtEmail.setText(mIntent.getStringExtra("Email"));
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        btUpdate = (Button) findViewById(R.id.btUpdate2);
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<PostPutDelUser> updateUserCall = mApiInterface.putUser(
+                        edtId.getText().toString(),
+                        edtNama1.getText().toString(),
+                        edtEmail.getText().toString(),
+                        edtNama2.getText().toString());
+
+                updateUserCall.enqueue(new Callback<PostPutDelUser>() {
+                    @Override
+                    public void onResponse(Call<PostPutDelUser> call, Response<PostPutDelUser> response) {
+                        MainActivity.ma.refresh();
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(Call<PostPutDelUser> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+        btDelete = (Button) findViewById(R.id.btDelete2);
+        btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtId.getText().toString().trim().isEmpty()==false){
+                    Call<PostPutDelUser> deleteUser = mApiInterface.deleteUser(edtId.getText().toString());
+                    deleteUser.enqueue(new Callback<PostPutDelUser>() {
+                        @Override
+                        public void onResponse(Call<PostPutDelUser> call, Response<PostPutDelUser> response) {
+                            MainActivity.ma.refresh();
+                            finish();
+                        }
+
+                        @Override
+                        public void onFailure(Call<PostPutDelUser> call, Throwable t) {
+                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btBack = (Button) findViewById(R.id.btBackGo);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.ma.refresh();
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/resvi/uasresvi/InsertActivity.java
@@ -0,0 +1,64 @@
+package com.resvi.uasresvi;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.resvi.uasresvi.Model.PostPutDelUser;
+import com.resvi.uasresvi.Rest.ApiClient;
+import com.resvi.uasresvi.Rest.ApiInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class InsertActivity extends AppCompatActivity {
+
+    EditText edtNama1, edtNama2,edtEmail;
+    Button btInsert, btBack;
+    ApiInterface mApiInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        edtNama1 = (EditText) findViewById(R.id.edtNama1);
+        edtNama2 = (EditText) findViewById(R.id.edtNama2);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        btInsert = (Button) findViewById(R.id.btInserting);
+        btInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<PostPutDelUser> postUserCall = mApiInterface.postUser(edtNama1.getText().toString(), edtNama2.getText().toString(), edtEmail.getText().toString());
+                postUserCall.enqueue(new Callback<PostPutDelUser>() {
+                    @Override
+                    public void onResponse(Call<PostPutDelUser> call, Response<PostPutDelUser> response) {
+                        MainActivity.ma.refresh();
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(Call<PostPutDelUser> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        btBack = (Button) findViewById(R.id.btBackGo);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.ma.refresh();
+                finish();
+            }
+        });
+    }
+}
